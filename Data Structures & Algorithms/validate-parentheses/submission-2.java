dde3353f1130ce26da63e7cf40b5bf93baba5ea5
devrajanchan <99471708+devrajanchan@users.noreply.github.com>
2026-04-22 11:39:09 +0530

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        if (s.length() % 2 != 0) return false;
+        for(char c:s.toCharArray())
+        {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } 
+            else {
+                if (stack.isEmpty()) return false;
+                char top = stack.pop();
+                if ((c == ')' && top != '(') || 
+                    (c == '}' && top != '{') || 
+                    (c == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+        if(stack.isEmpty()) return true;
+        else return false;
+    }
+}
